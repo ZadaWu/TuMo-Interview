150e92cdb8808a8d4c367767031fd27c3e8e9653
zada <zada.wu@gmail.com>
2024-12-07 15:01:22 +0800

    reuse Duplicate check

--- src/controllers/paymentCallbackController.ts
+++ src/controllers/paymentCallbackController.ts
@@ -19,7 +19,7 @@ export class PaymentCallbackController {
             }
 
             // 2. 检查是否重复通知
-            const isDuplicate = false; // await this.paymentCallbackService.isDuplicateNotification(req.body.transactionId + req.body.status );
+            const isDuplicate = await this.paymentCallbackService.isDuplicateNotification(req.body.transactionId + req.body.status );
             if (isDuplicate) {
                 console.log('Duplicate notification received', { transactionId: req.body.transactionId + req.body.status });
                 return res.status(200).send('success'); // 重复通知也返回成功
